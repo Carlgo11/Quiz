ff05bb9f39c5ee0898d4d31264f15d4222d65fae
Carlgo11 <carlgo11@users.noreply.github.com>
2022-05-13 20:19:11 +0200

    Add team name input

--- js/questions.js
+++ js/questions.js
@@ -1,23 +1,33 @@
+let user = window.localStorage.getItem('user');
+
 $(document).ready(function () {
-
-
+    if (user == null) {
+        let team_modal = new bootstrap.Modal($('#team-modal'))
+        team_modal.show();
+    }
 })
-let user = "team-b";
-$('.form-check-input').click(function (event) {
+$('#team-modal').submit(() => {
+    window.localStorage.setItem('user', $('#team').val());
+});
+let answers = JSON.parse(window.localStorage.getItem('answers')) || {};
+
+$('.form-check-input').click(function () {
     const [question, answer] = $(this).attr('id').split('-');
+    answers[question] = answer;
+    window.localStorage.setItem('answers', JSON.stringify(answers));
     console.log(answer);
     console.log(question);
-    sendAnswer(question, answer, user).then(r => console.log(r));
 })
-
-async function sendAnswer(question, answer, user) {
-    const data = {user: user, answers: {[question]: answer}}
+$('#send').click(function (){
+    sendAnswer().then(r => console.log(r));
+})
+async function sendAnswer() {
+    const data = {user: user, answers: answers}
     $.ajax({
         url: "https://quiz.carlgo11.workers.dev/",
         method: "POST",
         contentType: 'application/json',
         dataType: "json",
         data: JSON.stringify(data)
-
     })
 }
