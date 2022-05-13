3e26f5949ae396aa3ea6f79e6ede885887762012
Carlgo11 <carlgo11@users.noreply.github.com>
2022-05-13 22:22:46 +0200

    Clean up code

--- js/questions.js
+++ js/questions.js
@@ -1,42 +1,38 @@
 let user = window.localStorage.getItem('user');
+let answers = JSON.parse(window.localStorage.getItem('answers')) || {};
 
 $(document).ready(async function () {
     if (user == null) {
         let team_modal = new bootstrap.Modal($('#team-modal'))
-        await team_modal.show();
+        await team_modal.show()
     }
 })
 
 $('#team-modal').submit(() => {
-    window.localStorage.setItem('user', $('#team').val());
-});
-
-let answers = JSON.parse(window.localStorage.getItem('answers')) || {};
-
-$('.form-check-input').click(async function () {
-    const [question, answer] = $(this).attr('id').split('-');
-    answers[question] = answer;
-    window.localStorage.setItem('answers', JSON.stringify(answers));
-    console.log(answer);
-    console.log(question);
+    window.localStorage.setItem('user', $('#team').val())
 })
 
-$('#send').click(async function () {
-    await sendAnswer().then(r => {
-        $('.card').hide();
-        $('#send').hide();
+$('.form-check-input').click(async function () {
+    const [question, answer] = $(this).attr('id').split('-')
+    answers[question] = answer
+    window.localStorage.setItem('answers', JSON.stringify(answers))
+})
+
+$('#send').click(() => {
+    sendAnswer().then(r => {
+        $('.card,#send').hide();
         let done_modal = new bootstrap.Modal($('#done'))
         done_modal.show();
-    });
+    })
 })
 
 async function sendAnswer() {
     const data = {user: user, answers: answers}
     $.ajax({
-        url: "https://quiz.carlgo11.workers.dev/",
-        method: "POST",
+        url: 'https://quiz.carlgo11.workers.dev/',
+        method: 'POST',
         contentType: 'application/json',
-        dataType: "json",
+        dataType: 'json',
         data: JSON.stringify(data)
     })
 }
