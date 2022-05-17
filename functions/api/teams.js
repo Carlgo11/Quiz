77b9c706a88350e319ab421fb7070daa06c783ec
Carlgo11 <carlgo11@users.noreply.github.com>
2022-05-17 02:38:35 +0200

    Use env.AUTH_TOKEN for Bearer token

--- functions/api/teams.js
+++ functions/api/teams.js
@@ -28,6 +28,12 @@ async function storeAnswers(request, env) {
   }
 }
 
+export async function onRequestGet({request, env}) {
+  if (`Bearer ${env.AUTH_TOKEN}` === request.headers.get('Authorization'))
+    return await calculateAnswers(env)
+  return new Response(null, {status: 401})
+}
+
 async function calculateAnswers(env) {
   const _questions = await env.QUESTIONS.get('questions', {type: 'json'})
   const all_teams = await env.TEAMS.list({type: 'json'})
@@ -46,13 +52,6 @@ async function calculateAnswers(env) {
     }
   }
   return new Response(JSON.stringify(correct))
-}
-
-export async function onRequestGet({request, env}) {
-  if (`Bearer ${AUTH_TOKEN}` === request.headers.get('Authorization'))
-    return await calculateAnswers(env)
-  else
-    return new Response(null, {status: 403})
 }
 
 export async function onRequestPost({request, env}) {
