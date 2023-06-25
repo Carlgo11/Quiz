2da23f1c5e405c9b517f0fba57f06e252655ec31
Carlgo11 <carlgo11@users.noreply.github.com>
2023-06-25 23:01:57 +0200

    Add /api/questions

--- functions/api/teams.js
+++ functions/api/teams.js
@@ -1,36 +1,13 @@
 const jwt = require('@tsndr/cloudflare-worker-jwt')
 
-function getUserAnswers(user, userDB) {
-    return userDB.get(`user:${user}`, {type: "json"})
-        .then(userObject => userObject.answers || null);
-}
-
 async function createJWT(user) {
     const secret = crypto.randomUUID()
     const token = await jwt.sign({
-        user: user, nbf: Math.floor(Date.now() / 1000), // Not before: now
+        user: user,
+        nbf: Math.floor(Date.now() / 1000), // Not before: now
         exp: Math.floor(Date.now() / 1000) + (2 * (60 * 60)) // Expires: 2h
     }, secret)
     return {secret, token}
-}
-
-async function validateJWT(request, userDB) {
-    try {
-        const token = request.headers.get('Authorization').split('Bearer ')[1]
-        const {user} = jwt.decode(token)
-        const {secret} = await userDB.get(`user:${user}`, {type: 'json'})
-        if (await jwt.verify(token, secret)) return user
-    } catch (error) {
-        console.log(error)
-    }
-    return false;
-}
-
-export async function onRequestGet({request, env}) {
-    const userDB = env.USERS;
-    const user = await validateJWT(request, userDB)
-    if (!user) return new Response(null, {status: 401})
-    return new Response(JSON.stringify(getUserAnswers(user, userDB)))
 }
 
 export async function onRequestPut({request, env}) {
@@ -43,6 +20,7 @@ export async function onRequestPut({request, env}) {
         await userDB.put(`user:${user}`, JSON.stringify({secret: secret, answers: {}}))
         return new Response(JSON.stringify({token: token}), {status: 201})
     } catch (error) {
+        console.log(error)
         return new Response(null, {status: 500})
     }
 }
