7f86c4e36802d80b1d6f8f3875efdab709dfb6ad
Carlgo11 <carlgo11@users.noreply.github.com>
2023-07-07 02:46:24 +0200

    Change / page to route

---
+++ src/app/route.ts
@@ -0,0 +1,8 @@
+import {cookies} from 'next/headers';
+
+export const runtime = 'edge'
+
+export function GET() {
+    const location: string = !cookies().get('token') ? '/register' : '/questions';
+    return new Response(null, {status: 302, headers: {location: location}})
+}
