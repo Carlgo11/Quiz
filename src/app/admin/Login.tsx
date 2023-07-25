74047a1d44e41b167e8850959933149d4bfb917f
Carlgo11 <carlgo11@users.noreply.github.com>
2023-07-26 00:19:45 +0200

    Use basic auth for admin login

--- src/app/admin/Login.tsx
+++ src/app/admin/Login.tsx
@@ -10,27 +10,44 @@ async function sendForm(event: FormEvent, url: string) {
     event.preventDefault()
 
     const formData: FormData = new FormData(form) || {};
-    const res = await fetch(`${url}/admin`, {
-        method: 'POST', headers: {
-            'Content-Type': 'application/json', 'Accept': 'application/json',
-        }, body: JSON.stringify({
-            'user': formData.get('username'),
-            'password': formData.get('password')
-        }), cache: 'no-store'
-    })
+    // @ts-ignore
+    const errorElem: Element = document.querySelector('.error')
+    errorElem.textContent = ''
+    const auth = [formData.get('username'), formData.get('password')].join(':')
+    let data
+    try {
+        const res = await fetch(`${url}/admin`, {
+            method: 'POST', headers: {
+                'Accept': 'application/json',
+                'Authorization': `Basic ${Buffer.from(auth).toString('base64')}`
+            }, cache: 'no-store'
+        })
 
-    if (!res.ok) {
-        const {error} = await res.json();
-        console.error(`${res.status} ${res.statusText}: ${error}`)
-        // @ts-ignore
-        document.querySelector('.error').textContent = error
-        return null;
+        if (!res.ok) {
+            const {error} = await res.json();
+            errorElem.textContent = error
+            return false;
+        }
+
+        data = await res.json()
+
+        if (!data['token'] || !data['token']) throw new Error('No response from server.')
+
+    } catch (e) {
+        if (e instanceof TypeError) {
+            errorElem.textContent = "Unable to contact server. Try again later."
+        } else if (e instanceof Error) {
+            errorElem.textContent = e.message
+        }
+        console.error(e)
+        return false
     }
 
-    const {token} = await res.json();
+    const {token} = data
     // Set the session cookie
     const cookieValue = JSON.stringify({token});
     document.cookie = `token=${encodeURIComponent(cookieValue)}; path=/`;
+    console.log(token)
     return true;
 }
 
@@ -45,12 +62,12 @@ export default function LoginPage() {
                     async (e) => {
                         // @ts-ignore
                         if (await sendForm(e, process.env.NEXT_PUBLIC_API))
-                            await router.refresh()
+                            router.refresh()
                     }
                 }>
                     <h1 className={styles.header}>{tr.login}</h1>
                     <hr/>
-                    <input name="username" type="username" className="form-control mb-2" placeholder={tr.username} required pattern="[A-z0-9]{1,16}"/>
+                    <input name="username" type="username" className="form-control mb-2" placeholder={tr.username} required maxLength={16} pattern="[A-z0-9]{1,16}" title="Only letters and numbers allowed"/>
                     <input name="password" type="password" className="form-control mb-3" placeholder={tr.password} required minLength={1} maxLength={100}/>
                     <p className="error text-danger text-danger-emphasis mb-3">
                     </p>
